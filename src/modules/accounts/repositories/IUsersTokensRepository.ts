686ba856a41926260caeb5ec1459fddd4c335d89
rafaelmavale <rafael.marques.vale.dev@gmail.com>
2021-06-15 16:42:23 -0300

    finalizado refresh token com ethereal

--- src/modules/accounts/repositories/IUsersTokensRepository.ts
+++ src/modules/accounts/repositories/IUsersTokensRepository.ts
@@ -9,7 +9,8 @@ interface IUsersTokensRepository {
   }: ICreateUserTokenDTO): Promise<UserToken>;
 
   findByUserIdAndRefreshToken(user_id: string, refresh_token: string): Promise<UserToken>;
-  deleteById(id: string): Promise<void>;
+  deleteById(id: string): Promise<void>
+  findByRefreshToken(refresh_token: string): Promise<UserToken>;
 }
 
 export { IUsersTokensRepository }
--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -33,6 +33,13 @@ class DayjsDateProvider implements IDateProvider {
     return dayjs().add(days, "days").toDate();
 
   }
+
+  addHours(hours: number): Date {
+    return dayjs().add(hours, "hour").toDate();
+  }
+  compareIfBefore(start_date: Date, end_date: Date): boolean {
+    return dayjs(start_date).isBefore(end_date);
+  }
 }
 
 export { DayjsDateProvider }
